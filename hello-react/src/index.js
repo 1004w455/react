4fbdd5fe628bf34aceeecd74357d0cba196e1fcc
SangQ <2014ksg@adwitt.com>
2016-07-29 11:00:20 +0900

    fix: hot module 적용

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    rootEl
 );
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+        ReactDOM.render(
+            <NextApp />,
+            rootEl
+        );
+    });
+}
